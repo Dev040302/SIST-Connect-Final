f57f5bd55a03752885c29743a2a7dc002598b68a
M-Deepak-Sridharan <78675261+M-Deepak-Sridharan@users.noreply.github.com>
2023-03-29 18:43:04 +0530

    splash Animation

--- FYP/app/src/main/java/com/example/fyp/splashScreen.kt
+++ FYP/app/src/main/java/com/example/fyp/splashScreen.kt
@@ -5,11 +5,36 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.View
+import android.view.WindowManager
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.ActionBar
 
 class splashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        supportActionBar!!.hide()
         setContentView(R.layout.activity_splash_screen)
+
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+// Remember that you should never show the action bar if the
+// status bar is hidden, so hide that too if necessary.
+        actionBar?.hide()
+
+        var image = findViewById<ImageView>(R.id.imageView)
+        var logo = findViewById<TextView>(R.id.textView);
+        var slogan = findViewById<TextView>(R.id.textView2)
+
+        //Animations
+        var topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        var bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+        image.setAnimation(topAnim);
+        logo.setAnimation(bottomAnim);
+        slogan.setAnimation(bottomAnim);
 
         Handler(Looper.getMainLooper()).postDelayed({
             var intent: Intent = Intent(this,LoginActivity::class.java)
