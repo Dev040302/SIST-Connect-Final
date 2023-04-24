b58f64286f4bc37865166afe38d28c27d4e781ad
DeeviNagaManiKhanta <78676545+DeeviNagaManiKhanta@users.noreply.github.com>
2023-04-25 01:07:08 +0530

    quiz

---
+++ theQuiz/app/src/main/java/com/example/thequiz/QuizAttemptMain.kt
@@ -0,0 +1,11 @@
+package com.example.thequiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class QuizAttemptMain : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz_attempt_main)
+    }
+}
