7d099be69c2f97656749f9292fd6274295e9e80a
Dev Anand R <devanandrajasekar04@gmail.com>
2023-04-25 00:08:45 +0530

    quiz

---
+++ theQuiz/theQuiz/app/src/main/java/com/example/thequiz/AttemptQuiz.kt
@@ -0,0 +1,11 @@
+package com.example.thequiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AttemptQuiz : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_attempt_quiz)
+    }
+}
---
+++ theQuiz/theQuiz/app/src/main/java/com/example/thequiz/createQuiz.kt
@@ -0,0 +1,38 @@
+package com.example.thequiz
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.example.thequiz.databinding.ActivityCreateQuizBinding
+
+class createQuiz : AppCompatActivity() {
+     private lateinit var binding: ActivityCreateQuizBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivityCreateQuizBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnCreate.setOnClickListener {
+            if (binding.name.text.toString().isNotEmpty() && binding.noodques.text.toString().isNotEmpty()) {
+                val intent = Intent(this, saveQuestions::class.java)
+                val name=binding.name.text.toString()
+                val noofques=binding.noodques.text.toString()
+                intent.putExtra("Name",name)
+                intent.putExtra("NoOfQues",noofques)
+                startActivity(intent)
+                finish()
+
+            } else {
+                Toast.makeText(this, "Please enter your name or enter the no of questions", Toast.LENGTH_SHORT)
+                    .show()
+
+            }
+
+        }
+
+
+
+    }
+}
