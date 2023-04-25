0ff1c7e4e8621019dd688d10812a26edb054a5fa
Dev Anand R <devanandrajasekar04@gmail.com>
2023-04-25 13:23:32 +0530

    init

--- FYP/app/src/main/java/com/example/fyp/Student_Section/ChatFragment.kt
+++ FYP/app/src/main/java/com/example/fyp/Student_Section/ChatFragment.kt
@@ -5,13 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.anonymous_chat.ItemsViewModel
 import com.example.fyp.R
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.fragment_chat.*
 
 
 class ChatFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    var name:String = (activity as Student).name
+    var dept:String = (activity as Student).department
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,7 +35,41 @@ class ChatFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        recyclerview.layoutManager = LinearLayoutManager(activity)
+        var ref= Firebase.database.reference.child("Messages").child(dept)
 
+        ref.addValueEventListener( object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val data = ArrayList<ItemsViewModel>()
+                if (dataSnapshot.exists()) {
+                    for (d in dataSnapshot.getChildren()) {
+                        var key = d.key.toString()
+                        var id=dataSnapshot.child(key).child("id").getValue().toString()
+                        var msg = dataSnapshot.child(key).child("msg").getValue().toString()
+                        data.add(ItemsViewModel(id,msg))
+                    }
+                    val adapter = chatAdapter(data)
+
+                    recyclerview.adapter = adapter
+
+                    val adp=recyclerview.adapter
+
+                    recyclerview.smoothScrollToPosition(adp!!.itemCount-1)
+                }
+                else{
+                    var first=ref.push()
+                    first.child("id").setValue(dept)
+                    first.child("msg").setValue("This Thread is for $dept department")
+
+                }
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        } )
 
     }
 }
