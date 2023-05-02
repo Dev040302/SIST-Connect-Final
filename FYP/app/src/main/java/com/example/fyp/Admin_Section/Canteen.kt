6d0c0a2878761925a8ed86c0cb4b01411400d550
Dev Anand R <devanandrajasekar04@gmail.com>
2023-05-02 13:10:43 +0530

    activity

--- FYP/app/src/main/java/com/example/fyp/Admin_Section/Canteen.kt
+++ FYP/app/src/main/java/com/example/fyp/Admin_Section/Canteen.kt
@@ -1,12 +1,44 @@
 package com.example.fyp.Admin_Section
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import com.example.fyp.Data.items
 import com.example.fyp.R
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_admin1.*
 
 class Canteen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_canteen)
+
+        var button = findViewById<Button>(R.id.button)
+        var path = findViewById<EditText>(R.id.path)
+        var Name = findViewById<EditText>(R.id.Name)
+        var rate = findViewById<EditText>(R.id.rate)
+
+        button.setOnClickListener {
+
+            var mbase = FirebaseDatabase.getInstance().getReference().child("Items")
+            var item = items(Name.text.toString(),rate.text.toString().toInt())
+            mbase.child(path.text.toString()).push().setValue(item)
+        }
+
+        storebilling.setOnClickListener {
+
+            if(billnumber.text!=null) {
+
+                var intent: Intent = Intent(this, OrderBill::class.java)
+                intent.putExtra("orderId",billnumber.text.toString())
+                startActivity(intent)
+
+
+            }
+
+        }
+
     }
 }
